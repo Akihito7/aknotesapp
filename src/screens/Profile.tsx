d27e4281b5e6b135d8b5db2e73fa348af8977060
akihito7 <akihitopro7@gmail.com>
2023-11-28 14:39:17 -0300

    adicionado verificação nos input screen profile e scrollview

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -6,12 +6,15 @@ import {
     Input,
     Button,
     Text,
-    Toast
+    Toast,
+    FormControl,
+    ScrollView
 } from "native-base";
 import { useAuth } from "../contexts/AuthContext";
 
 import { api } from "../services/axios";
 import { useForm, Controller } from "react-hook-form"
+import * as yup from "yup";
 
 import { THEME } from "../theme";
 import { Octicons } from '@expo/vector-icons';
@@ -22,6 +25,7 @@ import EmailSvg from "../../assets/E-mail.svg";
 
 import profile from "../../assets/profile.png"
 import AppError from "../utils/AppError";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 
 type PasswordProps = {
@@ -30,9 +34,17 @@ type PasswordProps = {
 }
 export function Profile() {
 
+    const Schema = yup.object().shape({
+        currentPassword: yup.string().min(6, "A senha deve conter no minimo 6 dígitos").required("O campo não pode estar vazio"),
+        newPassword: yup.string().min(6, "A senha deve conter no minimo 6 dígitos").required("O campo não pode estar vazio")
+    })
+
     const { user } = useAuth();
 
-    const { control, handleSubmit } = useForm();
+    const { control,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({ resolver: yupResolver(Schema) });
 
     async function handleUpdatePassword({ currentPassword, newPassword }: PasswordProps) {
         try {
@@ -71,241 +83,261 @@ export function Profile() {
     }
 
     return (
-        <VStack
-            flex={1}
-            bg="gray.700"
-
+        <ScrollView
+            contentContainerStyle={{ flexGrow: 1 }}
+            showsVerticalScrollIndicator={false}
 
         >
-            <HStack
-                mt={16}
-                justifyContent="space-between"
-                py={8}
-                px={8}
-            >
-
-                <TouchableOpacity activeOpacity={0.2}>
-                    <Octicons
-                        name="arrow-left"
-                        size={28}
-                        color={THEME.colors.gray[100]} />
-                </TouchableOpacity>
-
-                <TouchableOpacity activeOpacity={0.2} >
-
-                    <Ionicons
-                        name="ios-power-sharp"
-                        size={28}
-                        color={THEME.colors.gray[100]}
-                    />
-                </TouchableOpacity>
-            </HStack>
-
-
             <VStack
                 flex={1}
-                mt={12}
-                px={8}
-                py={8}
-                borderTopRadius={50}
-                alignItems="center"
-                bg="gray.500"
+                bg="gray.700"
+
+
             >
-                <Image
-                    source={profile}
-                    defaultSource={profile}
-                    borderRadius={99}
-                    height={32}
-                    width={32}
-                    resizeMode="contain"
-                    alt="foto de perfil"
-
-                    position="absolute"
-                    top={"-10%"}
-                    left="41%"
-                />
                 <HStack
-                    alignItems="center"
-                    height={65}
-                    bg="gray.700"
-                    borderColor="transparent"
-                    borderWidth={2}
-                    borderRadius={10}
-                    mb={4}
-                    px={5}
-                    mt={24}
+                    mt={16}
+                    justifyContent="space-between"
+                    py={8}
+                    px={8}
                 >
-                    <NomeSvg height={24} width={24} />
 
-                    <Input
-                        flex={1}
-                        bg="gray.700"
-                        borderColor="transparent"
-                        fontFamily="body"
-                        fontSize="sm"
-                        color="white.100"
-                        placeholderTextColor="gray.300"
-                        value={user?.name}
-                        isReadOnly={true}
-                    />
+                    <TouchableOpacity activeOpacity={0.2}>
+                        <Octicons
+                            name="arrow-left"
+                            size={28}
+                            color={THEME.colors.gray[100]} />
+                    </TouchableOpacity>
 
+                    <TouchableOpacity activeOpacity={0.2} >
+
+                        <Ionicons
+                            name="ios-power-sharp"
+                            size={28}
+                            color={THEME.colors.gray[100]}
+                        />
+                    </TouchableOpacity>
                 </HStack>
 
-                <HStack
 
+
+
+                <VStack
+                    flex={1}
+                    mt={12}
+                    px={8}
+                    py={8}
+                    borderTopRadius={50}
                     alignItems="center"
-                    height={65}
-                    bg="gray.700"
-                    borderColor="transparent"
-                    borderWidth={2}
-                    borderRadius={10}
-                    mb={4}
-                    px={5}
-
+                    bg="gray.500"
                 >
+                    <Image
+                        source={profile}
+                        defaultSource={profile}
+                        borderRadius={99}
+                        height={32}
+                        width={32}
+                        resizeMode="contain"
+                        alt="foto de perfil"
 
-                    <EmailSvg height={24} width={24} />
-                    <Input
-                        flex={1}
+                        position="absolute"
+                        top={"-10%"}
+                        left="41%"
+                    />
+                    <HStack
+                        alignItems="center"
+                        height={65}
                         bg="gray.700"
                         borderColor="transparent"
-                        fontFamily="body"
-                        fontSize="sm"
-                        color="white.100"
-                        placeholderTextColor="gray.300"
-                        value={user?.email}
-                        isReadOnly={true}
-                    />
-
-                </HStack>
-
-                <Controller
-                    name="currentPassword"
-                    control={control}
-                    render={({ field: { onChange, value } }) => {
-
-                        return (
-
-
-                            <HStack
-                                alignItems="center"
-                                height={65}
-                                bg="gray.700"
-                                borderColor="transparent"
-                                borderWidth={2}
-                                borderRadius={10}
-                                mb={4}
-                                px={5}
-                                mt={8}
-                            >
-                                < PasswordSvg width={24} height={24} />
-
-                                <Input
-                                    flex={1}
-                                    onChangeText={onChange}
-                                    value={value}
-                                    type="password"
-                                    bg="gray.700"
-                                    borderColor="transparent"
-                                    fontFamily="body"
-                                    fontSize="sm"
-                                    color="white.100"
-                                    placeholderTextColor="gray.300"
-                                    placeholder="Current password"
-
-                                    _focus={{
-                                        bg: "gray.700",
-                                        borderColor: "gray.700"
-                                    }}
-
-                                />
-
-                            </HStack>
-
-                        )
-
-                    }}
-
-                />
-
-                <Controller
-                    name="newPassword"
-                    control={control}
-                    render={({ field: { onChange, value } }) => {
-
-                        return (
-
-                            <HStack
-                                alignItems="center"
-                                height={65}
-                                bg="gray.700"
-                                borderColor="transparent"
-                                borderWidth={2}
-                                borderRadius={10}
-                                mb={4}
-                                px={5}
-                            >
-                                < PasswordSvg width={24} height={24} />
-
-
-                                <Input
-                                    flex={1}
-                                    onChangeText={onChange}
-                                    value={value}
-                                    type="password"
-                                    bg="gray.700"
-                                    borderColor="transparent"
-                                    fontFamily="body"
-                                    fontSize="sm"
-                                    color="white.100"
-                                    placeholderTextColor="gray.300"
-                                    placeholder="New password"
-
-                                    _focus={{
-                                        bg: "gray.700",
-                                        borderColor: "gray.700"
-                                    }}
-
-                                />
-                            </HStack>
-
-                        )
-
-                    }}
-                />
-
-
-                <Button
-                    onPress={handleSubmit(handleUpdatePassword)}
-                    alignItems="center"
-                    justifyContent="center"
-                    mt={8}
-                    width="full"
-                    height={65}
-                    bg="orange.700"
-                    borderRadius={10}
-                    borderWidth={0}
-                    px={12}
-                    py={2}
-
-                    _pressed={{
-                        opacity: "0.8",
-                        bg: "orange.700"
-                    }}
-
-                >
-                    <Text
-                        fontFamily="heading"
-                        fontSize='xl'
+                        borderWidth={2}
+                        borderRadius={10}
+                        mb={4}
+                        px={5}
+                        mt={24}
                     >
-                        Save
-                    </Text>
-                </Button>
+                        <NomeSvg height={24} width={24} />
+
+                        <Input
+                            flex={1}
+                            bg="gray.700"
+                            borderColor="transparent"
+                            fontFamily="body"
+                            fontSize="sm"
+                            color="white.100"
+                            placeholderTextColor="gray.300"
+                            value={user?.name}
+                            isReadOnly={true}
+                        />
+
+                    </HStack>
+
+                    <HStack
+
+                        alignItems="center"
+                        height={65}
+                        bg="gray.700"
+                        borderColor="transparent"
+                        borderWidth={2}
+                        borderRadius={10}
+                        mb={4}
+                        px={5}
+
+                    >
+
+                        <EmailSvg height={24} width={24} />
+                        <Input
+                            flex={1}
+                            bg="gray.700"
+                            borderColor="transparent"
+                            fontFamily="body"
+                            fontSize="sm"
+                            color="white.100"
+                            placeholderTextColor="gray.300"
+                            value={user?.email}
+                            isReadOnly={true}
+                        />
+
+                    </HStack>
+
+                    <Controller
+                        name="currentPassword"
+                        control={control}
+                        render={({ field: { onChange, value } }) => {
+
+                            const message = errors.currentPassword?.message || "Erro, tente novamente";
+
+                            const invalid = !!errors.currentPassword?.message || false;
+
+                            return (
+
+                                <FormControl isInvalid={invalid} >
+
+                                    <HStack
+                                        alignItems="center"
+                                        height={65}
+                                        bg="gray.700"
+                                        borderColor={invalid ? "red.700" : "transparent"}
+                                        borderWidth={2}
+                                        borderRadius={10}
+                                        mb={4}
+                                        px={5}
+                                        mt={8}
+                                    >
+                                        < PasswordSvg width={24} height={24} />
+
+                                        <Input
+                                            flex={1}
+                                            onChangeText={onChange}
+                                            value={value}
+                                            borderWidth={0}
+                                            type="password"
+                                            bg="gray.700"
+
+                                            fontFamily="body"
+                                            fontSize="sm"
+                                            color="white.100"
+                                            placeholderTextColor="gray.300"
+                                            placeholder="Current password"
+
+                                            _focus={{
+                                                bg: "gray.700",
+                                                borderColor: "gray.700"
+                                            }}
+
+                                        />
+
+                                    </HStack>
+                                    <FormControl.ErrorMessage mt={-2} mb={2}>
+                                        {message}
+                                    </FormControl.ErrorMessage>
+                                </FormControl>
+                            )
+                        }}
+                    />
+
+                    <Controller
+                        name="newPassword"
+                        control={control}
+                        render={({ field: { onChange, value } }) => {
+
+                            const message = errors.newPassword?.message || "Erro, tente novamente"
+
+                            const invalid = !!errors.newPassword?.message || false
+
+                            return (
+
+                                <FormControl isInvalid={invalid}>
+                                    <HStack
+                                        alignItems="center"
+                                        height={65}
+                                        bg="gray.700"
+                                        borderColor={invalid ? "red.700" : "transparent"}
+                                        borderWidth={2}
+                                        borderRadius={10}
+                                        mb={4}
+                                        px={5}
+                                    >
+                                        < PasswordSvg width={24} height={24} />
 
 
+                                        <Input
+                                            flex={1}
+                                            onChangeText={onChange}
+                                            value={value}
+                                            type="password"
+                                            bg="gray.700"
+                                            borderWidth={0}
+                                            fontFamily="body"
+                                            fontSize="sm"
+                                            color="white.100"
+                                            placeholderTextColor="gray.300"
+                                            placeholder="New password"
 
-            </VStack>
+                                            _focus={{
+                                                bg: "gray.700",
+                                                borderColor: "gray.700"
+                                            }}
 
-        </VStack>
+                                        />
+                                    </HStack>
+
+                                    <FormControl.ErrorMessage mt={-2} mb={2}>
+                                        {message}
+                                    </FormControl.ErrorMessage>
+                                </FormControl>
+                            )
+                        }}
+                    />
+
+
+                    <Button
+                        onPress={handleSubmit(handleUpdatePassword)}
+                        alignItems="center"
+                        justifyContent="center"
+                        mt={8}
+                        width="full"
+                        height={65}
+                        bg="orange.700"
+                        borderRadius={10}
+                        borderWidth={0}
+                        px={12}
+                        py={2}
+
+                        _pressed={{
+                            opacity: "0.8",
+                            bg: "orange.700"
+                        }}
+
+                    >
+                        <Text
+                            fontFamily="heading"
+                            fontSize='xl'
+                        >
+                            Save
+                        </Text>
+                    </Button>
+                </VStack>
+
+            </VStack >
+        </ScrollView >
     )
 }
